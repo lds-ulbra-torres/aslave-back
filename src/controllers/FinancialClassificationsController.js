7c20d8fb3938f4508eadf3fc16e6032afa67e2ed
JozielOliveira <programdor.joziel@gmail.com>
2019-02-26 11:43:42 -0300

    financial releases and classifications

---
+++ src/controllers/FinancialClassificationsController.js
@@ -0,0 +1,45 @@
+import db from '../config/db'
+import { response, error } from './API'
+
+const FinancialClassificationsModel = db().models.fin_classifications
+
+
+export const FinancialClasificarionsController  = {
+    index(req, res) {
+        FinancialClassificationsModel.findAll()
+        .then(result => response( res, erro))
+        .catch( erro => error(res, erro) )
+    },
+
+    get (req,res){
+        FinancialClassificationsModel.findAll({
+            where:{
+                id_classification: req.parms.id
+            }
+        })
+        .then( result => response(res,result) )
+        .catch(erro => error (res,err) )
+    },
+
+    store(req,res){
+        FinancialClassificationsModel.create(req.body)
+        .then( result => response(res,result) )
+        .catch(erro => error (res,erro) )
+
+    },
+
+    update(req,res){
+        if(req.body.id_classification){
+            error(res, {}, 'id_classification')}
+        else{
+            FinancialClassificationsModel.update
+            (req, body, { where:{
+                id_classification:req.params.id
+            }})
+            .then( result => response (res, result) )
+            .catch(erro =>error (res,erro) )
+        }   
+    
+    },
+    
+}
---
+++ src/models/FinancialClassificationsModel.js
@@ -0,0 +1,33 @@
+export default (sequelize, DataType)=> {
+    const FinancialClassifications = sequelize.define('fin_classifications', {
+        id_classification: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowIncrement: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        
+        name_classification:{
+            type: DataType.STRING(50),
+            allowNull: false,
+            validate:{
+                notEmpty: true,
+            },
+        },
+
+        classifation_type:{
+            type: DataType.STRING(1),
+            allowNull: false,
+            validate:{
+                notEmpty: true,
+            },
+        },
+        
+    })
+
+    return FinancialClassifications
+}
